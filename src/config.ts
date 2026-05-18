6e56b6127651ee88dce0d83d53ab1b35db12d48a
Jon Gallant <2163001+jongio@users.noreply.github.com>
2026-05-18 07:35:12 -0700

    fix: decompose Header, extract config, async fonts, fix README prop (#94)
    - Decompose Header.astro god component into NavLinks, SocialLinks, MobileMenu sub-components (#82)
    - Extract hardcoded author/social URLs into src/config.ts (#83)
    - Make Google Fonts non-render-blocking with media=print/onload pattern (#61)
    - Fix README HeroBanner prop name: title -> titleHtml (#56)

---
+++ src/config.ts
@@ -0,0 +1,9 @@
+export const author = {
+  name: "Jon Gallant",
+  twitter: "@jongallant",
+};
+
+export const social = {
+  x: "https://x.com/jongallant",
+  linkedin: "https://www.linkedin.com/in/intenseagile/",
+};
